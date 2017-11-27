bbee09cc70d20fe6b5dc78ec3b85f550a9eba1bc
mjmersenski <mjmersenski@mflaptop01.qg.com>
2017-11-27 14:04:20 -0600

    made a third comment for this commit

--- src/practice/Startup.java
+++ src/practice/Startup.java
@@ -8,6 +8,7 @@ public class Startup {
     public static void main(String[] args) {
         System.out.println("Test message for GIT practice.");
         System.out.println("Making a second commit");
+        System.out.println("Making a third commit");
     }
     
 }
