f606d022462509f454875a89a56212033af57dfa
mjmersenski <mjmersenski@mflaptop01.qg.com>
2017-11-27 13:54:04 -0600

    this is a practice project for GIT training

---
+++ src/practice/Startup.java
@@ -0,0 +1,12 @@
+package practice;
+
+/**
+ *
+ * @author mjmersenski
+ */
+public class Startup {
+    public static void main(String[] args) {
+        System.out.println("Test message for GIT practice.");
+    }
+    
+}
