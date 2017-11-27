08462843582dc8099ccaeab7a71fefbfef2e1eb8
mjmersenski <mjmersenski@mflaptop01.qg.com>
2017-11-27 14:03:43 -0600

    made a second comment for this commit

--- src/practice/Startup.java
+++ src/practice/Startup.java
@@ -7,6 +7,7 @@ package practice;
 public class Startup {
     public static void main(String[] args) {
         System.out.println("Test message for GIT practice.");
+        System.out.println("Making a second commit");
     }
     
 }
